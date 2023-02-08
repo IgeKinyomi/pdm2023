527b76e302f3b9d2b46a9b4aa5b4e773a71e36cf
IgeKinyomi <123094843+IgeKinyomi@users.noreply.github.com>
2023-02-07 22:16:15 -0600

    editing speed etc

--- sprite animation/sketch.js
+++ sprite animation/sketch.js
@@ -1,140 +1,140 @@
-let spriteSheet;
-let chronoSheet;
-
-let walkingAnimation;
-let walkingAnimation2;
-let chronoAnimation;
-
-let spriteSheetFilenames = ["SpelunkyGuy.png", "Green.png", "Blue.png", "Ninja.png"];
-let spriteSheets = [];
-let totalAnimations = 10;
-let animations = [];
+let spelunkyCharater, greenCharacter, ninjaCharacter, blueCharacter;
+let array = [];
 
 function preload() {
-  for(let i=0; i < spriteSheetFilenames.length; i++) {
-    spriteSheets[i] = loadImage("assets/" + spriteSheetFilenames[i]);
-  }
+ //loading sprite sheets 
+   spelunkyCharater = loadImage("../spritecharacters/SpelunkyGuy.png");
+   blueCharacter = loadImage("../spritecharacters/Blue.png");
+   greenCharacter = loadImage("../spritecharacters/Green.png");
+   ninjaCharacter = loadImage("../spritecharacters/Ninja.png"); 
 }
 
 function setup() {
-  createCanvas(400, 400);
-  imageMode(CENTER);
-  angleMode(DEGREES);
+  createCanvas(window.innerWidth, window.innerHeight);
+    // scale problem (-1)
+    imageMode(CENTER);
 
-  for(let i=0; i < totalAnimations; i++) {
-    animations[i] = new WalkingAnimation(random(spriteSheets),80,80,random(100,300),random(100,300),9,random(0.5,1),6);
-  }
+    spelunkyCharater = new Character(spelunkyChar, random(50, window.innerWidth-50), random(window.innerHeight/4 - 30, window.innerHeight - 40), random(2, 5));
+    blueCharacter = new Character(blueChar, random(50, window.innerWidth-50), random(window.innerHeight/4 - 30, window.innerHeight - 40), random(2, 5));
+    greenCharacter = new Character(greenChar, random(50, window.innerWidth-50), random(window.innerHeight/4 - 30, window.innerHeight - 40), random(2, 5));
+    ninjaCharacter = new Character(ninjaChar, random(50, window.innerWidth-50), random(window.innerHeight/4 - 30, window.innerHeight - 40), random(2, 5));
+    
+    let choices = [35, 70]
+    for(let i = 0; i < 20; i++) {
+      array[i] = new Cloud(random(0, window.innerWidth), 70, random(1, 2));
+    }
+    for(let i = 20; i < 40; i++) {
+      array[i] = new Cloud(random(0, window.innerWidth), 35, random(1, 2));
+    }
+    
 }
-
 function draw() {
   background(220);
 
-  for(let i=0; i < animations.length; i++) {
+  blueCharacter.draw();
+  greenCharacter.draw();
+  spelunkyCharater.draw();
+  ninjaCharacter.draw();
+
+  for(let i=0; i < 10; i++) {
     animations[i].draw();
   }
 }
+function keyPressed() {
+  if(keyCode == RIGHT_ARROW) {
+    greenCharacter.go(1);
+    blueCharacter.go(1);
+    spelunkyCharacter.go(1);
+    ninjaCharacter.go(1);
 
-function mousePressed() {
-  for (let i=0; i < animations.length; i++) {
-    let contains = animations[i].contains(mouseX,mouseY);
-    if (contains) {
-      if (animations[i].moving != 0)
-        animations[i].stop();
-      else {
-        if (animations[i].xDirection === 1)
-          animations[i].moveRight();
-        else
-          animations[i].moveLeft();
-      }
-    }
+  } else if (keyCode == LEFT_ARROW) {
+    greenCharacter.go(1);
+    blueCharacter.go(1);
+    spelunkyCharacter.go(1);
+    ninjaCharacter.go(1);
+
   }
 }
 
-class WalkingAnimation {
-  constructor(spritesheet, sw, sh, dx, dy, animationLength, speed, framerate, offsetX = 0, offsetY = 0) {
-    this.spritesheet = spritesheet;
-    this.sw = sw;
-    this.sh = sh;
-    this.dx = dx;
-    this.dy = dy;
-    this.u = 0;
-    this.v = 0;
-    this.animationLength = animationLength;
-    this.currentFrame = 0;
-    this.moving = 1;
-    this.xDirection = 1;
-    this.offsetX = offsetX;
-    this.offsetY = offsetY;
+function keyReleased() {
+  greenCharacter.go(1);
+  blueCharacter.go(1);
+  spelunkyCharacter.go(1);
+  ninjaCharacter.go(1);
+}
+
+class Cloud {
+  constructor(x, y, speed) {
+    this.x = x;
+    this.y = y;
     this.speed = speed;
-    this.framerate = framerate*speed;
   }
 
   draw() {
+    fill(255);
+    noStroke();
+    ellipse(this.x, this.y, 40, 20);
+    ellipse(this.x+30, this.y, 40, 20);
+    ellipse(this.x+60, this.y, 40, 20);
+    ellipse(this.x+15, this.y-10, 40, 20);
+    ellipse(this.x+45, this.y-10, 40, 20);
 
-    // if (this.moving != 0)
-    //   this.u = this.currentFrame % this.animationLength;
-    // else
-    //   this.u = 0;
+    this.x += this.speed;
 
-    this.u = (this.moving != 0) ? this.currentFrame % this.animationLength : 0;
+    if(this.x > window.innerWidth + 100) {
+      this.x = -100;
+    }
+  }
+
+}
+
+class Character {
+  constructor(character, x, y, speed) {
+    this.character = character;
+    this.x = x;
+    this.y = y;
+    this.move = 0;
+    this.facing = 1;
+    this.speed = speed;
+  }
+
+  draw() {
     push();
-    translate(this.dx,this.dy);
-    rotate(90);
-    scale(this.xDirection,1);
+    translate(this.x,this.y);
+    scale(this.facing, 1);
+
+    if(this.move == 0) {
+      image(this.character, 0, 0, 80, 80, 0, 0, 80, 80);
+    } else {
+      image(this.character, 0, 0, 80, 80, 80 * (this.sx + 1), 0, 80, 80);
+    }
 
 
-    rect(-26,-35,50,70);
+    if(frameCount % (7 - (round(this.speed - 2))) == 0) {
+      this.sx = (this.sx + 1) % 8;
+    }
 
-    image(this.spritesheet,0,0,this.sw,this.sh,this.u*this.sw+this.offsetX,this.v*this.sh+this.offsetY,this.sw,this.sh);
+    if(this.x > window.innerWidth) {
+      this.move = -(this.move);
+      this.facing = -(this.facing);
+    }
+
+    if(this.x < 0) {
+      this.move = -(this.move);
+      this.facing = -(this.facing);
+    }
+
+    this.x += this.speed * this.move;
     pop();
-    let proportionalFramerate = round(frameRate() / this.framerate);
-    if (frameCount % proportionalFramerate == 0) {
-      this.currentFrame++;
-    }
-
-    this.dx += this.moving*this.speed;
-
-    if (this.dx > width - this.sw / 4) {
-      this.moveLeft();
-    } else if (this.dx < this.sw / 4) {
-      this.moveRight();
-    }
   }
 
-  moveRight() {
-    this.moving = 1;
-    this.xDirection = 1;
-  }
-
-  moveLeft() {
-    this.moving = -1;
-    this.xDirection = -1;
-  }
-
-  keyPressed(right, left) {
-    if (keyCode === right) {
-
-      this.currentFrame = 1;
-    } else if (keyCode === left) {
-
-      this.currentFrame = 1;
-    }
-  }
-
-  keyReleased(right,left) {
-    if (keyCode === right || keyCode === left) {
-      this.moving = 0;
-    }
-  }
-
-  contains(x,y) {
-    //rect(-26,-35,50,70);
-    let insideX = x >= this.dx - 26 && x <= this.dx + 25;
-    let insideY = y >= this.dy - 35 && y <= this.dy + 35;
-    return insideX && insideY;
+  go(direction) {
+    this.move = direction;
+    this.facing = direction;
+    this.sx = 3;
   }
 
   stop() {
-    this.moving = 0;
+    this.move = 0;
   }
 }
