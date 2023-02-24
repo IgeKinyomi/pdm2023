2a9b870020ed6385acf004e17f18fcb7d7acd52e
IgeKinyomi <123094843+IgeKinyomi@users.noreply.github.com>
2023-02-23 19:09:03 -0600

    added new sounds
    created new sounds
    fixed code

--- build-a-sampler/js/sketch.js
+++ build-a-sampler/js/sketch.js
@@ -1,16 +1,54 @@
 //Ige Kinyomi 2023
 
-let sound1 = new Tone.Player("sounds/chicken.wav");
+let sounds = new Tone.Players({
+
+  "cheer": "sounds/cheer.mp3",
+  "clap": "sounds/clap.mp3",
+  "jolly": "sounds/jolly.mp3",
+  "drop": "sounds/water.mp3"
+
+})
+
+const delay = new Tone.FeedbackDelay("8n", 0.5);
+
+let soundNames = ["cheer", "clap", "jolly", "drop"];
+let buttons = [];
+
+let dSlider;
+let fSlider;
+
+// let button1, button2, button3;
 
 function setup() {
   createCanvas(400, 400);
-  sound1.toDestination();
+  sounds.connect(delay);
+  delay.toDestination();
+
+  soundNames.forEach((word, index) => {
+    buttons[index] = createButton(word);
+    buttons[index].position(index, index*50);
+    buttons[index].mousePressed( () => buttonSound(word))
+  })
+
+  dSlider = createSlider(0., 1., 0.5, 0.05);
+  dSlider.mouseReleased( () => {
+    delay.delayTime.value = dSlider.value();
+  })
+
+  fSlider = createSlider(0., 1., 0.5, 0.05);
+  fSlider.mouseReleased( () => {
+    delay.feedback.value = fSlider.value();
+  })
+
+
 }
 
 function draw() {
-background (220);
+  background(220, 120, 180);
+  text('press the buttons for sound', 0, 150)
+
 }
 
-function keyPressed() {
-  sound1.start();
+function buttonSound(whichSound) {
+    sounds.player(whichSound).start();
 }
