9ee68ddf1075813b75231d52ef40a5cfaab90e7c
IgeKinyomi <123094843+IgeKinyomi@users.noreply.github.com>
2023-03-01 21:58:50 -0600

    changed reverb
    changed reverb to chorus

--- synthandsequence/in class sound ex/js/sketch.js
+++ synthandsequence/in class sound ex/js/sketch.js
@@ -12,10 +12,10 @@ const metal = new Tone.MetalSynth({
   "resonance" : 300 ,
   "octaves" : 1.5
 });
-const reverb = new Tone.JCReverb(0.4).toDestination();
-synth.connect(reverb); 
-drum.connect(reverb);
-metal.connect(reverb);
+const chorus = new Tone.Chorus(4, 2.5, 0.5).toDestination().start();
+synth.connect(chorus); 
+drum.connect(chorus);
+metal.connect(chorus);
 // code base
 //Key notes connecting with keyboard letters as a piano
 let notes = {
@@ -39,9 +39,9 @@ function setup() {
 //synth.harmonicity.value = 1.25
 //play middle 'C' for duration of 8th note
 synth.triggerAttackRelease("C4", "8n");
-//change value of reverb room size
+//change value of chorus room size
 slider.on('change', (v) => {
-  reverb.roomSize.value = v;
+  chorus.roomSize.value = v;
 })
 }
 
