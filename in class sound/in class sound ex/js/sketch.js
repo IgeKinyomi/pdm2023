b5b30ec4190d8a37039c1d90eb9cef8e51f393cb
IgeKinyomi <123094843+IgeKinyomi@users.noreply.github.com>
2023-03-01 20:15:08 -0600

    updating code
    updating code

--- in class sound/in class sound ex/js/sketch.js
+++ in class sound/in class sound ex/js/sketch.js
@@ -1,4 +1,21 @@
-const synth = new Tone.Synth():
+const synth = new Tone.Synth();
+const drum = new Tone.MembraneSynth();
+const metal = new Tone.MetalSynth({
+  "frequency" : 45 ,
+  "envelope" : {
+    "attack" : 0.001 ,
+    "decay" : 0.4 ,
+    "release" :0.2
+  } ,
+  "harmonicity" : 8.5 ,
+  "modulationIndex" : 40 ,
+  "resonance" : 300 ,
+  "octaves" : 1.5
+});
+const reverb = new Tone.JCReverb(0.4).toDestination();
+synth.connect(reverb); 
+drum.connect(reverb);
+metal.connect(reverb);
 // code base
 //Key notes connecting with keyboard letters as a piano
 let notes = {
@@ -16,23 +33,24 @@ let notes = {
 
 function setup() {
   createCanvas(400, 400);
-  synth.toDestination();
+  synth.release = 2;
+  synth.resonance = 0.98;
+//synth.harmonicity.value = 1.25
+//play middle 'C' for duration of 8th note
+synth.triggerAttackRelease("C4", "8n");
 }
-
-function mousePressed() {
-  Tone.start();
-}
-
 
 //Don't put sounds in the draw function, it will make it weird
 function draw() {
   background(220);
 }
+//console.log toPlay
 functionkeyPressed(){
-  let whatNote = notes[key]
-
-  //console.log whatNot
-synth.triggerAttackRelease(whatNote, "8n")  
+  let toPlay = notes[key];
+  console.log(toPlay);
+  synth.triggerAttackRelease(toPlay,0.5);
+  metal.triggerAttackRelease("C3","8n",'+0.5' );
+  drum.triggerAttackRelease("C2","8n", '+1' );
 }
 
 
