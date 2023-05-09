145bc15dab033d6026570575b03a76f8c8bf1137
IgeKinyomi <123094843+IgeKinyomi@users.noreply.github.com>
2023-05-08 20:23:43 -0500

    Update sketch.js

--- piratequest/sketch.js
+++ piratequest/sketch.js
@@ -1,110 +1,163 @@
-//canvas
-function setup() {
-  createCanvas(600, 200);
-  frameRate(60);
-  textSize(24);
-}
-
-//game variables
+let player;
+let ground;
+let obstacles = [];
+let coins = [];
 let score = 0;
 let lives = 3;
-let timer = 0;
-let gameState = "welcome";
+let isGameOver = false;
+let isPlaying = false;
+let gameStartTime;
+let gameEndTime;
 
-// define game objects
-let player;
-let obstacles = [];
-
-// welcome screen
-function drawWelcomeScreen() {
-  background(255);
-  fill(0);
-  textAlign(CENTER);
-  text("Press Space to Start", width / 2, height / 2);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(200, 200, 200);
+  player = new Player();
+  ground = new Ground();
 }
 
-// game over screen
-function drawGameOverScreen() {
-  background(255);
-  fill(0);
-  textAlign(CENTER);
-  text("Game Over!", width / 2, height / 2 - 30);
-  text("Score: " + score, width / 2, height / 2);
-  text("Press Space to Restart", width / 2, height / 2 + 30);
-}
-
-//game loop
-function drawGame() {
-  background(255);
-  
-  // update the player
-  player.update();
-  player.show();
-  
-  // update and show the obstacles
-  for (let i = obstacles.length - 1; i >= 0; i--) {
-    obstacles[i].update();
-    obstacles[i].show();
-    
-    // check for collision with player
-    if (obstacles[i].hits(player)) {
-      lives--;
-      obstacles.splice(i, 1);
-      
-      // end game if no lives left
-      if (lives <= 0) {
-        gameState = "gameover";
+function draw() {
+  if (isPlaying) {
+    clear();
+    background(200, 200, 200);
+    player.show();
+    player.update();
+    ground.show();
+    if (frameCount % 60 == 0) {
+      let obstacle = new Obstacle();
+      obstacles.push(obstacle);
+      if (frameCount % 120 == 0) {
+        let coin = new Coin();
+        coins.push(coin);
       }
     }
-    
-    // remove obstacles that have gone off screen
-    if (obstacles[i].offscreen()) {
-      obstacles.splice(i, 1);
-      score++;
+    for (let i = obstacles.length - 1; i >= 0; i--) {
+      obstacles[i].show();
+      obstacles[i].update();
+      if (obstacles[i].hits(player)) {
+        lives--;
+        if (lives == 0) {
+          gameOver();
+        }
+        obstacles.splice(i, 1);
+      } else if (obstacles[i].offscreen()) {
+        obstacles.splice(i, 1);
+        score++;
+      }
     }
-  }
-  
-  // add new obstacles
-  if (frameCount % 60 == 0) {
-    obstacles.push(new Obstacle());
-  }
-  
-  //game info
-  fill(0);
-  textAlign(LEFT);
-  text("Score: " + score, 20, 30);
-  text("Lives: " + lives, 20, 60);
-  text("Time: " + timer, 20, 90);
-  
-  // update timer
-  if (frameCount % 60 == 0) {
-    timer++;
+    for (let i = coins.length - 1; i >= 0; i--) {
+      coins[i].show();
+      coins[i].update();
+      if (coins[i].hits(player)) {
+        coins.splice(i, 1);
+        score += 10;
+      } else if (coins[i].offscreen()) {
+        coins.splice(i, 1);
+      }
+    }
+    textSize(20);
+    text(`Score: ${score}`, 10, 30);
+    text(`Lives: ${lives}`, 10, 60);
+  } else if (isGameOver) {
+    clear();
+    textSize(40);
+    textAlign(CENTER);
+    text("Game Over", width / 2, height / 2 - 20);
+    textSize(20);
+    text(`Score: ${score}`, width / 2, height / 2 + 20);
+  } else {
+    clear();
+    textSize(40);
+    textAlign(CENTER);
+    text("Press space to start", width / 2, height / 2 - 20);
+    textSize(20);
+    text(`Score: ${score}`, width / 2, height / 2 + 20);
   }
 }
 
-// handle keyboard input
+class Ground {
+  constructor() {
+    this.x1 = 0;
+    this.y1 = height - 50;
+    this.x2 = width;
+    this.y2 = height;
+  }
+  
+  show() {
+    fill(0, 200, 0); // set the ground color to green
+    rect(this.x1, this.y1, this.x2, this.y2);
+  }
+}
+
 function keyPressed() {
-  if (keyCode == 32) {
-    if (gameState == "welcome") {
-      player = new Player();
-      gameState = "playing";
-    } else if (gameState == "gameover") {
-      score = 0;
-      lives = 3;
-      timer = 0;
-      obstacles = [];
-      gameState = "welcome";
+  if (keyCode === 32) { // spacebar
+    if (isGameOver) {
+      restartGame();
+    } else if (!isPlaying) {
+      startGame();
+    } else {
+      player.jump();
+    }
+  } else if (keyCode === UP_ARROW) {
+    if (!isPlaying) {
+      startGame();
+    } else {
+      player.jump();
     }
   }
 }
 
-// define the main game loop
-function draw() {
-  if (gameState == "welcome") {
-    drawWelcomeScreen();
-  } else if (gameState == "playing") {
-    drawGame();
-  } else if (gameState == "gameover") {
-    drawGameOverScreen();
+function startGame() {
+  isPlaying = true;
+  gameStartTime = millis();
+}
+
+function gameOver() {
+  isGameOver = true;
+  isPlaying = false;
+  gameEndTime = millis();
+}
+
+function restartGame() {
+  isGameOver = false;
+  isPlaying = true;
+  score = 0;
+  lives = 3;
+  player = new Player();
+  obstacles = [];
+  coins = [];
+  gameStartTime = millis();
+}
+
+class Player {
+  constructor() {
+    this.x = 50;
+    this.y = height - 50;
+    this.width = 50;
+    this.height = 50;
+    this.yVelocity = 0;
+    this.gravity = 1.5;
+  }
+  
+  show() {
+    fill(0);
+    rect(this.x, this.y, this.width, this.height);
+  }
+  
+  update() {
+    this.yVelocity += this.gravity;
+    this.y += this.yVelocity;
+    
+    // check for floor collision
+    if (this.y + this.height > height) {
+      this.y = height - this.height;
+      this.yVelocity = 0;
+    }
+  }
+  
+  jump() {
+    if (this.y + this.height == height) {
+      this.yVelocity = -25;
+    }
   }
 }
